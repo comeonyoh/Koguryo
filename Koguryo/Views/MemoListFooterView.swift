111d7c1e50c7bd2310eeb8fdada46b1468315407
KimJungSu <comeonyoh89@gmail.com>
2016-11-16 18:47:12 +0900

    Logo image changed

---
+++ Koguryo/Views/MemoListFooterView.swift
@@ -0,0 +1,21 @@
+//
+//  MemoListFooterView.swift
+//  Koguryo
+//
+//  Created by KimJungSu on 11/16/16.
+//  Copyright © 2016 ODOV. All rights reserved.
+//
+
+import UIKit
+
+class MemoListFooterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
