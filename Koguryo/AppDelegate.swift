5f3615f0df9c14ddfebc0869e0a8434046c59629
comeonyoh <comeonyoh89@gmail.com>
2017-07-05 14:30:46 +0900

    Firebase analytics added

--- Koguryo/AppDelegate.swift
+++ Koguryo/AppDelegate.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+        -> Bool {
+
+            FirebaseApp.configure()
+            return true
+    }
+
 }
 
